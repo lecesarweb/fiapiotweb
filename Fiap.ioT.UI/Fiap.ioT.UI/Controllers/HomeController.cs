b843dd37a5cb4a3508f71ed2742cf9aad85a4bd3
Leandro Silva <lecesarweb@gmail.com>
2018-03-11 14:58:44 -0300

    Fiap.ioT.Web
    Publicação do projeto Web(MCV) consumindo API de integração com Arduino

---
+++ Fiap.ioT.UI/Fiap.ioT.UI/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Fiap.ioT.UI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Fiap.ioT.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Localhost
+        //string Baseurl = "http://localhost:32138/";
+
+        //WEB
+        string Baseurl = "http://fiapiotapi.azurewebsites.net/";
+        public async Task<ActionResult> Index()
+        {
+            List<Medida> EmpInfo = new List<Medida>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage Res = await client.GetAsync("api/medidas");
+
+                if (Res.IsSuccessStatusCode)
+                { 
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result; 
+                    EmpInfo = JsonConvert.DeserializeObject<List<Medida>>(EmpResponse);
+
+                }
+                return View(EmpInfo);
+            }
+        }
+    }
+}
---
+++ Fiap.ioT.UI/Fiap.ioT.UI/Models/Medida.cs
@@ -0,0 +1,8 @@
+﻿namespace Fiap.ioT.UI.Models
+{
+    public class Medida
+    {
+        public int? Id { get; set; }
+        public string Valor { get; set; }
+    }
+}
